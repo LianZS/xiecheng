f8f43cf5ef85433d67ef1fe4308e25c4e8e152e1
lianzongsheng <2214057556@qq.com>
2020-02-21 16:52:08 +0800

    csv文件追加操作功能完成

--- src/export_file.py
+++ src/export_file.py
@@ -18,8 +18,8 @@ class FileModel(Enum):
 class ExportFile(object):
     __data_file__ = None  # 文件实例
     row = 0  # 行数
-    worksheet = None  # 表
-    add_adjust = False  # 用来判断是否获取过一次行数
+    __worksheet__ = None  # 表
+    __copy_adjust__ = False  # 用来判断是否获取过一次行数
 
     def __init__(self, file_name: str, file_type: FileType, write_model: FileModel = FileModel.WB):
         """
@@ -32,13 +32,13 @@ class ExportFile(object):
         self.file_name = ''.join([file_name, '.', file_type.value])
         self.write_model = write_model
         if write_model == FileModel.ADD:
-            self.add_adjust = True  # 可以获取文本行数
+            self.__copy_adjust__ = True  # 可以获取文本行数
 
-    def write_to_file(self, data: list, sheet_name='My Worksheet'):
+    def write_to_file(self, data: list, sheet_name: str = 'My Worksheet'):
         if self.file_type == FileType.XLS or self.file_type == FileType.XLSX:
             if self.__data_file__ is None:
                 self.__data_file__ = xlwt.Workbook(encoding='ascii')
-                self.worksheet = self.__data_file__.add_sheet(sheetname=sheet_name)
+                self.__worksheet__ = self.__data_file__.add_sheet(sheetname=sheet_name)
             if self.file_type == FileType.XLS:
 
                 for c, label in enumerate(data):
@@ -54,51 +54,66 @@ class ExportFile(object):
                         self.__data_file__ = open(self.file_name, mode='a+', newline='')
                     elif self.write_model == FileModel.WB:
                         self.__data_file__ = open(self.file_name, mode='w+', newline='')
-                    self.worksheet = csv.writer(self.__data_file__)
+                    self.__worksheet__ = csv.writer(self.__data_file__)
 
                 else:
                     open(self.file_name, 'x').close()
                     self.write_to_file(data, sheet_name)
+                    return
             self.__write_to_csv__(data)
 
-    def add_data(self, data: list, sheet_name=None):
+    def add_data(self, data: list, sheet_name: str = None):
         """
         在文件原有数据基础上进行追加
         :param data: 数据列表
         :return:
         """
+        # 判断文件是否已经存在，不存在则创建后写入
+        if not os.path.exists(self.file_name):
+            if sheet_name is None:
+                self.write_to_file(data)
+            else:
+                self.write_to_file(data, sheet_name)
+            self.save()
+            return
         if self.file_type == FileType.XLS or self.file_type == FileType.XLSX:
-            if not os.path.exists(self.file_name):
-                if sheet_name is None:
-                    self.write_to_file(data)
-                else:
-                    self.write_to_file(data, sheet_name)
-                self.save()
-                return
 
-            if self.add_adjust:  # 确保文件只读取复制一次
+            if self.__copy_adjust__:  # 确保文件只读取复制一次
                 book = xlrd.open_workbook(self.file_name)
-                self.__data_file__ = copy(book)  # 完成xlrd对象向xlwt对象转换
+                self.__data_file__: xlwt.Workbook = copy(book)  # 完成xlrd对象向xlwt对象转换
                 if sheet_name is None:
                     sheet = book.sheet_by_index(0)
-                    self.worksheet = self.__data_file__.get_sheet(0)
+                    self.__worksheet__ = self.__data_file__.get_sheet(0)
 
                 else:
                     try:
                         sheet = book.sheet_by_name(sheet_name)
-                        self.worksheet = self.__data_file__.get_sheet(sheet_name)
+                        self.__worksheet__ = self.__data_file__.get_sheet(sheet_name)
 
                     except xlrd.XLRDError:
                         sheet = book.sheet_by_index(0)
-                        self.worksheet = self.__data_file__.get_sheet(0)
+                        self.__worksheet__ = self.__data_file__.get_sheet(0)
                 self.row = sheet.nrows  # 获得行数
-                self.add_adjust = False  # 后面的追加操作不能再读取行数了。
+                self.__copy_adjust__ = False  # 后面的追加操作不能再读取行数了。
 
-            if sheet_name is None:
-                self.write_to_file(data)
-            else:
+        elif self.file_type == FileType.CSV:
+            if self.__copy_adjust__:  # 确保文件只读取复制一次
+                # 读取行数
+                with open(self.file_name, mode='r') as r:
+                    self.row = len([i for i in csv.reader(r)])
 
-                self.write_to_file(data, sheet_name)
+                if self.write_model == FileModel.ADD:
+                    self.__data_file__: TextIOWrapper = open(self.file_name, mode='a+', newline='')
+                elif self.write_model == FileModel.WB:
+                    self.__data_file__: TextIOWrapper = open(self.file_name, mode='w+', newline='')
+                self.__worksheet__ = csv.writer(self.__data_file__)
+                self.__copy_adjust__ = False
+
+        if sheet_name is None:
+            self.write_to_file(data)
+        else:
+
+            self.write_to_file(data, sheet_name)
 
     def __write_to_xls__(self, row, colum, label):
         """
@@ -108,7 +123,7 @@ class ExportFile(object):
         :param label: 数据
         :return:
         """
-        self.worksheet.write(row, colum, label)
+        self.__worksheet__.write(row, colum, label)
         self.row += 1
 
     def __write_to_xlsx__(self, row, colum, label):
@@ -119,18 +134,17 @@ class ExportFile(object):
         :param label: 数据
         :return:
         """
-        self.worksheet.write(row, colum, label)
+        self.__worksheet__.write(row, colum, label)
         self.row += 1
 
     def __write_to_csv__(self, data: list):
         """
-        写入xls
-        :param row: 第几行
-        :param colum: 第几列
-        :param label: 数据
+        写入csv文件
+        :param data:数据列表
         :return:
         """
-        self.worksheet.writerow(data)
+        self.__worksheet__.writerow(data)
+        self.row += 1
 
     def save(self):
         """
@@ -142,7 +156,8 @@ class ExportFile(object):
             self.__data_file__.save(self.file_name)
 
         elif self.file_type == FileType.CSV:
-            self.__data_file__.close()
+            if not self.__copy_adjust__:
+                self.__data_file__.close()
 
     @property
     def fieldnames(self):
